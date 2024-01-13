d97873d81e86f358b1a8a00618a65506e99b10db
AndrejVenas <84439808+AndrejVenas@users.noreply.github.com>
2024-01-13 23:52:04 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var mixer = mixitup('.portfolio__gallery');
+$('.slider-blog__inner').slick({
+  dots: true,
+  nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="next arrow"></button>',
+  prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="next arrow"></button>'
+});
