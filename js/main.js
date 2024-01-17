081d26785deeecc5511cc412f93952d9cddd8e3a
AndrejVenas <84439808+AndrejVenas@users.noreply.github.com>
2024-01-17 23:22:35 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
-var mixer = mixitup('.portfolio__gallery');
+$('.menu__btn').click(function(){
+  $('.menu__list').toggleClass('menu__list--active');
+});
 $('.slider-blog__inner').slick({
   dots: true,
   nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="next arrow"></button>',
-  prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="next arrow"></button>'
+  prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="next arrow"></button>',
+  responsive: [
+    {
+      breakpoint: 651,
+      settings: {
+        arrows: false
+      }
+    }
+  ]
 });
+var mixer = mixitup('.portfolio__gallery');
